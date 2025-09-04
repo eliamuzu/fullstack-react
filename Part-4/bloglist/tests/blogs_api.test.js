f59645c979b21af098c1b90909c90bf10763c1a2
eliamuzu <kofiprince375@gmail.com>
2025-09-04 22:57:15 +0000

    tests for post endpoint

--- Part-4/bloglist/tests/blogs_api.test.js
+++ Part-4/bloglist/tests/blogs_api.test.js
@@ -1,50 +1,20 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
-const { identifierCheck } = require('../utils/blog_helper')
-
+const helper = require('../utils/blogtest_helper')
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-  {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  },
-  {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-  },
-]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blog = new Blog(initialBlogs[0])
-  await blog.save()
-  blog = new Blog(initialBlogs[1])
-  await blog.save()
-  blog = new Blog(initialBlogs[2])
-  await blog.save()
-  blog = new Blog(initialBlogs[3])
-  await blog.save()
+
+  for (const blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
 })
 
 
@@ -69,6 +39,56 @@ test('unique identifier of blog is named id not _id', async()=>{
     assert.ok(blog.hasOwnProperty('id'), "There is and '_id' property in a blog")
   })
 })
+
+test( 'post request to "/api/blogs" creates a new blog', async() => {
+  const newBlog =  {
+    title: "Does the post test work",
+    author: "Prince W. Test",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  };
+
+  await api.post('/api/blogs').send(newBlog)
+
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+})
+
+test('if likes property is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: "Blog without likes",
+    author: "Test Author",
+    url: "http://example.com"
+    // likes is missing
+  }
+
+  const response = await api.post('/api/blogs').send(newBlog)
+  assert.strictEqual(response.body.likes, 0)
+})
+
+describe('new blogs are not missing data', () => {
+
+  test('like title', async () => {
+  const newBlog = {
+    author: "Test Author",
+    url: "http://example.com"
+  }
+
+  const response = await api.post('/api/blogs').send(newBlog)
+  assert.strictEqual(response.status, 400)
+  })
+
+  test('like url', async () => {
+    const newBlog = {
+      title: "Blog without url",
+      author: "Test Author"
+    }
+
+    const response = await api.post('/api/blogs').send(newBlog)
+    assert.strictEqual(response.status, 400)
+  })
+})
+
 
 after(async () =>{
   await mongoose.connection.close()
